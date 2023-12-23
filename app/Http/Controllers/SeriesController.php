823bc8afa2bd6f2f2a86747598b7df2e3256ba03
Franklin Nascimento <waddinsohn@gmail.com>
2023-12-23 19:31:16 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = $request->session()->get('mensagem.sucesso');
+
+        return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        Serie::create($request->all());
+
+        return to_route('series.index');
+    }
+
+    public function destroy(Request $request)
+    {
+        Serie::destroy($request->series);
+        $request->session()->put('mensagem.sucesso', 'Série removida com suecesso!');
+
+        return to_route('series.index');
+    }
+}
